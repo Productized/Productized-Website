b6f679ffdb0f23f93dbedb9ea72873570a9e4052
fabbud <fabbud@gmail.com>
2020-05-24 22:34:46 +0100

    current event is responsive, need logo with red background

--- src/components/Homepage/Homepage.js
+++ src/components/Homepage/Homepage.js
@@ -1,13 +1,22 @@
 import React from 'react';
 import HomepageCarousel from './Carousel/HomepageCarousel';
+import CurrentEvent from './CurrentEvent/CurrentEvent';
 
-function Homepage() {
-  return (
-    <>
-      <HomepageCarousel />
-      <h1>Homepage</h1>
-    </>
-  );
+class Homepage extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+  render() {
+    return (
+      <>
+        <HomepageCarousel />
+        <CurrentEvent />
+        <h1>Homepage</h1>
+      </>
+    );
+  }
 }
 
 export default Homepage;
