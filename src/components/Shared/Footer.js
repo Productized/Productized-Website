529fe2e3be598bb993ce4e948881859a19145e37
fabbud <fabbud@gmail.com>
2020-06-24 23:55:24 +0100

    FOOTER: a:visited css added and target _blank added | NAVBAR: links color changed to a lighter gray | ALBUMCARD: css changed to productized red, cards are responsive | YEARCARDS: changed class into function, deleted state, created a json file adding the local images, used require in src of img to ref local images | VIDCARD, RECAPCARD and PREVIOUS CONFERENCES: added local images to json file, used require in src of img to ref local images

--- src/components/Shared/Footer.js
+++ src/components/Shared/Footer.js
@@ -36,6 +36,8 @@ function Footer() {
           <a
             className="link"
             href="https://www.google.com/maps/place/R.+de+Campolide+28,+1070-028+Lisboa,+Portugal/@38.7261916,-9.16429,17z/data=!3m1!4b1!4m5!3m4!1s0xd19336eb3637e3b:0x6e2427db7fe066ed!8m2!3d38.7261916!4d-9.1621013"
+            target="_blank"
+            rel="noopener noreferrer"
           >
             Rua Campolide nº28 1070-028 Lisboa
           </a>
@@ -56,6 +58,8 @@ function Footer() {
             <a
               className="social-links"
               href="https://www.facebook.com/ProductizedConf/"
+              target="_blank"
+              rel="noopener noreferrer"
             >
               <img
                 className="facebook"
@@ -68,6 +72,8 @@ function Footer() {
             <a
               className="social-links"
               href="https://twitter.com/productizedconf"
+              target="_blank"
+              rel="noopener noreferrer"
             >
               <img
                 className="twitter"
@@ -80,6 +86,8 @@ function Footer() {
             <a
               className="social-links"
               href="https://www.instagram.com/productized.co/"
+              target="_blank"
+              rel="noopener noreferrer"
             >
               <img
                 className="instagram"
@@ -92,6 +100,8 @@ function Footer() {
             <a
               className="social-links"
               href="https://www.linkedin.com/school/productized/"
+              target="_blank"
+              rel="noopener noreferrer"
             >
               <img
                 className="linkedin"
