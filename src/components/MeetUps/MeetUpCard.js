ea52af2800b737ea7d0931d1ef6d1463f7f8faf5
fabbud <fabbud@gmail.com>
2020-06-26 10:49:52 +0100

    all meetups thumbnails added to assets/meetups folder, meetupsdatabase.json file created including local images names and a boolean isDisplayed to allow to choose which cards to be displayed, filter to allow this choice added in meetupslist component, banner for meetups page title as a suggested design and meetup cards in flexbox

--- src/components/MeetUps/MeetUpCard.js
+++ src/components/MeetUps/MeetUpCard.js
@@ -2,18 +2,19 @@ import React from 'react';
 import './MeetUps.css';
 
 function MeetUpCard(props) {
+  const meetups = props;
   return (
     <div className="meetupcard-div">
       <div className="meetup-img">
         <img
-          className="meetup-cardimg"
-          src={props.cardImage}
-          alt={props.albumTitle}
+          className="meetup-card-img"
+          src={require(`../../assets/meetups/${meetups.imageLocal}`)}
+          alt={meetups.albumTitle}
         />
-        <a href={props.videoURL} target="_blank" rel="noopener noreferrer">
+        <a href={meetups.videoURL} target="_blank" rel="noopener noreferrer">
           <p className="youtube">YOUTUBE</p>
         </a>
-        <a href={props.albumURL} target="_blank" rel="noopener noreferrer">
+        <a href={meetups.albumURL} target="_blank" rel="noopener noreferrer">
           <p className="flickr">FLICKR</p>
         </a>
       </div>
--- src/components/MeetUps/MeetUpsList.js
+++ src/components/MeetUps/MeetUpsList.js
@@ -1,57 +1,33 @@
 import React from 'react';
-import { Tabs, Tab } from 'react-bootstrap';
 import NavBar from '../Shared/NavBar';
 import Footer from '../Shared/Footer';
 import MeetUpsDatabase from './MeetUpsDatabase.json';
 import './MeetUps.css';
 import MeetUpCard from './MeetUpCard';
 
-class MeetUps extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = { chosenMeetUps: [], key: '', years: [2020, 2019, 2018] };
-  }
-  componentDidMount() {
-    let chosenMeetUps = MeetUpsDatabase.filter(
-      (meetup) => meetup.year === '2020',
-    );
-    this.setState({ chosenMeetUps, key: '2020' });
-  }
-
-  handleChoice(eventKey) {
-    let key = eventKey;
-    let chosenMeetUps = MeetUpsDatabase.filter(
-      (meetup) => meetup.year === eventKey,
-    );
-    this.setState({ chosenMeetUps, key });
-    console.log(this.state.chosenMeetUps);
-  }
-
-  render() {
-    const { chosenMeetUps, years } = this.state;
-    return (
-      <div>
-        <NavBar />
+function MeetUps() {
+  const chosenMeetUps = MeetUpsDatabase.filter(
+    (meetup) => meetup.isDisplayed === true,
+  );
+  return (
+    <div>
+      <NavBar />
+      <div className="meetup-title-div">
+        <img
+          className="meetup-background"
+          src={require(`../../assets/meetups/P_Talk_BackgroundImage.jpg`)}
+          alt="P-Talks Background"
+        />
         <h1 className="meetup-title">MeetUps</h1>
-        <Tabs
-          id="controlled-tab-example"
-          activeKey={this.state.key}
-          onSelect={(eventKey) => this.handleChoice(eventKey)}
-        >
-          {years.map((year) => (
-            <Tab eventKey={year} title={year}>
-              <div className="tab-div">
-                {chosenMeetUps.map((meetup) => (
-                  <MeetUpCard {...meetup} />
-                ))}
-              </div>
-            </Tab>
-          ))}
-        </Tabs>
-        <Footer />
       </div>
-    );
-  }
+      <div className="meetup-div">
+        {chosenMeetUps.map((meetup) => (
+          <MeetUpCard {...meetup} />
+        ))}
+      </div>
+      <Footer />
+    </div>
+  );
 }
 
 export default MeetUps;
